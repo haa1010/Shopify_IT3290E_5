fa4fa2fb93a2c89d2d6593a6d0a2b5c8c96d5c18
haa1010 <hang40338@gmail.com>
2020-05-27 23:26:35 +0700

    add cors

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors());
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
-app.use(bodyParser.json());
 
 // app.use(function (error, req, res, next) {
 //   if(error instanceof SyntaxError){ //Handle SyntaxError here.
